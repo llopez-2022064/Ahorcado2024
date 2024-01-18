c8c40d879e8d81042b1b92db24fe1ca7ea33a2e1
Informatica <informatica@alumno.local>
2024-01-18 17:06:22 -0600

    Version 3

---
+++ Version 3/AhorcadoSexto/funciones.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+//x, y , ancho ,alto
+ctx.arc(150, 30, 20, 0, 2 * Math.PI);
+
+//Cuerpo
+ctx.moveTo(150, 50, 100, 250);
+ctx.lineTo(150, 150, 100, 250);
+
+//Brazo derecho
+ctx.moveTo(150, 50, 100, 250);
+ctx.lineTo(100, 80, 100, 250);
+
+//Brazo izquierdo
+ctx.moveTo(150, 50, 100, 250);
+ctx.lineTo(200, 75, 100, 250);
+
+//Pierna izquierda
+ctx.moveTo(150, 150, 100, 250);
+ctx.lineTo(195, 190, 100, 250);
+
+//Pierna izquierda
+ctx.moveTo(150, 150, 100, 250);
+ctx.lineTo(100, 190, 100, 250);
+
+ctx.stroke();
